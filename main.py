25f601929ded373188e4bdda3e4df15c4c8aae9a
Denís Bermúdez Delgado <denis.bermudez@dxcfds.com>
2021-04-23 11:59:12 +0200

    Correxido erro no csv

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import schedule
+from time import sleep
 
 from obterdatos import dato
 from chiador import chia
 
+schedule.every().day.at("10:55").do(print(dato()))
+
 if __name__ == '__main__':
-	tweet = dato()
-	chia(tweet)
+	while True:
+		schedule.run_pending()
+		sleep(1)
